6be2c09c5b9f906872de375264b4d4aad122e9ae
Avera Guthrie <averaguthrie@gmail.com>
2020-02-04 20:01:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,21 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+
+var addElementToBeginningOfArray = function(array, element) {
+  var array = []
+  [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+  array.unshift(element)
+}
+
+function addElementToEndOfArray() {
+  [...array, element]
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+  array.pop(element)
+}
