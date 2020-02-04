dea37dfd586b5b4cada298e8719e75140a5316bd
Avera Guthrie <averaguthrie@gmail.com>
2020-02-04 20:20:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,22 +9,22 @@ accessElementInArray();
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 
-}
+};
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
-}
+};
 
 function addElementToEndOfArray(array, element) {
   return [...array, element]
-}
+};
 
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
-}
+};
 
-// function accessElementInArray(array. index) {
-//   return array[i];
-// }
+function accessElementInArray(array. index) {
+  return array[i];
+};
