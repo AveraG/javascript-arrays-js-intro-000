ee7d6eb8d56444404c25b21baa49fd048100badd
Avera Guthrie <averaguthrie@gmail.com>
2020-02-04 20:36:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,6 +35,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 };
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice()
-  return array;
+  //array.slice()
+  return array.slice(1)
 }
